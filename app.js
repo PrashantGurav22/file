125ab5e8534948aeca50415eeb150ae2edfb4329
PrashantGurav22 <prashantgurav7953@gmail.com>
2025-01-29 21:12:41 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function add(a,b)
+{
+    return a+b
+}
+function msg()
+{
+    msg="it is abeautiful"
+    return msg
+}
+let name="kle"
+module.exports={add,msg}
---
+++ index.js
@@ -0,0 +1,14 @@
+// import express module 
+const express=require('express')
+const app=express()
+
+app.use(express.json())
+    
+let usres=[
+    id:1,name:"viraj",email:"abcd@gmail.com"
+    {}
+}]
+
+})
+//start the server
+app.listen(3000,()=>console.log('server is running in the port on 3000'))
